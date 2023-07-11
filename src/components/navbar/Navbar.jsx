f5614e2771859b1a87b4d5c72afe8884539530ef
Ollie <c0087087@newcastle.ac.uk>
2023-07-11 13:04:47 +0700

    Fixed gradient position

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -6,9 +6,7 @@ import gradientImage from '../../assets/gradient.png';
 const Navbar = () => {
   return (
    <div className='ow_navbar'>
-    <div className='background'>
-        <img src={gradientImage} alt='Gradient' className='gradient-image' />
-      </div>
+    
     <div className='ow_navbar-links'>
       <div className='ow_navbar-links-logo'>
         <img src={logojpg} alt='logo' className='logo-image' />
@@ -19,6 +17,9 @@ const Navbar = () => {
       <p><a href='#skills'>.skills</a></p>
       <p><a href='#experience'>.experience</a></p>
       <p><a href='#contact'>.contacts</a></p>
+      </div>
+      <div className='background'>
+        <img src={gradientImage} alt='Gradient' className='gradient-image' />
       </div>
     </div>
    </div>
