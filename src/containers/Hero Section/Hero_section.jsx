4f34bed32e73fd01147cf9d5bd500eb23de89044
Ollie <c0087087@newcastle.ac.uk>
2023-07-12 01:38:26 +0700

    Adding Extra packages - Gsap, Jquery

--- src/containers/Hero Section/Hero_section.jsx
+++ src/containers/Hero Section/Hero_section.jsx
@@ -1,6 +1,7 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import './Hero_section.css';
 import monitor from '../../assets/monitor.png';
+import '../../animations/animation.js' // Import the animation file
 // import monitorimage from '../../assets/blob1.svg'
 
 
@@ -11,18 +12,18 @@ const Hero_section = () => {
         <h1>
           <span className='small-text'>Hi, I'm</span>
           <br />
-          <span className='big-text'> Oliver Wilde</span> 
-          <br/>
+          <span className='big-text'> DONT TOUCH MY CODE</span>
+          <br />
           <span className='subheading'> Aspring Student@NCL University</span>
         </h1>
         {/* <div className='ow_hero-section-monitor-images'>
           <img src = {monitorimage} alt='monitorimage' className='monitor-image'></img>
         </div> */}
-        <div className='ow_hero-section-monitor'>
-          <img src={monitor} alt='monitor' class="monitor-image"/>
+        <div className='ow_hero- section-monitor'>
+          <img src={monitor} alt='monitor' class="monitor-image" />
         </div>
-        
-        
+
+
       </div>
     </div>
   )
