04771cfd9c52ce2934f2a8be0efdcc85b0115311
Ollie <c0087087@newcastle.ac.uk>
2023-07-11 16:37:45 +0700

    Implemented Monitor image

--- src/containers/Hero Section/Hero_section.jsx
+++ src/containers/Hero Section/Hero_section.jsx
@@ -1,11 +1,24 @@
-import React from 'react'
-
+import React, {useState} from 'react';
 import './Hero_section.css';
-
+import monitor from '../../assets/monitor.png';
 
 const Hero_section = () => {
   return (
-    <div>Hero_section</div>
+    <div className='ow_hero-section-padding'>
+      <div className='ow_gpt3_hero-section-content'>
+        <h1>
+          <span className='small-text'>Hi, I'm</span>
+          <br />
+          <span className='big-text'> Oliver Wilde</span> 
+          <br/>
+          <span className='subheading'> Aspring Student@NCL University</span>
+        </h1>
+        <div className='ow_hero-section-monitor'>
+          <img src={monitor} alt='monitor' class="monitor-image"/>
+        </div>
+        
+      </div>
+    </div>
   )
 }
 
