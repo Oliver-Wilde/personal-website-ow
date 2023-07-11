2a71843e0474eac1e57601d050487bd55d917349
Ollie <c0087087@newcastle.ac.uk>
2023-07-11 09:50:05 +0700

    setting up structure - subject to change

---
+++ src/containers/Hero Section/Hero_section.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+import './Hero_section.css';
+
+
+const Hero_section = () => {
+  return (
+    <div>Hero_section</div>
+  )
+}
+
+export default Hero_section
---
+++ src/containers/index.js
@@ -0,0 +1,2 @@
+export {default as Hero_section} from "./Hero Section/Hero_section";
+export {default as About_me} from "./About Me/About_me";
