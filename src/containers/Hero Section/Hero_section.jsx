a44e5aa3bb42903f7ef76ef656af8c6ad010a36d
Ollie <c0087087@newcastle.ac.uk>
2023-07-12 15:24:49 +0700

    remove break

--- src/containers/Hero Section/Hero_section.jsx
+++ src/containers/Hero Section/Hero_section.jsx
@@ -8,7 +8,7 @@ import '../../animations/animation.js' // Import the animation file
 const Hero_section = () => {
   return (
     <div className='ow_hero-section-padding'>
-      <div className='ow_gpt3_hero-section-content'>
+      <div className='ow_hero-section-content'>
         <h1>
           <span className='small-text'>Hi, I'm</span>
           <br />
@@ -17,9 +17,10 @@ const Hero_section = () => {
           <span className='subheading'> Aspiring Student@NCL University</span>
           <div className='resume_button_container'>
           <button type='button'>Resume</button>
+          <button className='contact_button' type='button'>Contact</button>
         </div>
         </h1>
-        <br />
+        
         
         <div className='ow_hero-section-monitor-padding'>
           <div className='ow_hero-section-monitor'>
