53490d28e31e8adf9bd166e018d1635e568ec357
Ollie <c0087087@newcastle.ac.uk>
2023-07-22 14:04:31 +0700

    implement hover effect using switch method

--- src/containers/Hero Section/Hero_section.jsx
+++ src/containers/Hero Section/Hero_section.jsx
@@ -22,12 +22,53 @@ const Hero_section = () => {
         </h1>
         <div className='ow_hero-section-monitor-padding'>
           <div className='ow_hero-section-monitor'>
-            <img src={monitor} alt='monitor' class="monitor-image" />
+            {/* <img src={monitor} alt='monitor' class="monitor-image" /> */}
+            <img src={about} alt='about' class="about-gif"/>
           </div>
         </div>
       </div>
     </div>
   );
 };
+
+// scripts.js
+const imageContainer = document.querySelector('.ow_hero-section-monitor');
+const image = imageContainer.querySelector('img');
+
+const links = document.querySelectorAll('ow_navbar-links-container a');
+
+links.forEach(link => {
+  link.addEventListener('mouseover', () => {
+    const newImagePath = getNewImagePath(link.textContent);
+    if (newImagePath) {
+      image.src = newImagePath;
+    }
+  });
+
+  link.addEventListener('mouseout', () => {
+    // Change the image back to the default image on mouseout
+    image.src = 'C:/Users/james/personal-website-ow/src/assets/About_me.gif';
+  });
+});
+
+function getNewImagePath(linkText) {
+  // Implement your logic to determine the new image path based on the link text
+  // For example, you can use a switch statement or an object with link-text-to-image-path mappings
+  switch (linkText) {
+    case '.portfolio':
+      return 'C:/Users/james/personal-website-ow/src/assets/Portfolio_hover.gif';
+    case '.about':
+      return 'path-to-about-image.jpg';
+    case '.skills':
+      return 'path-to-services-image.jpg';
+    case '.experience':
+      return 'path-to-services-image.jpg';  
+    case '.contacts':
+      return 'path-to-services-image.jpg';
+    // Add more cases for additional links
+    default:
+      return null;
+  }
+}
 
 export default Hero_section;
