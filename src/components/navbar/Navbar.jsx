51f407bd7e4b30ba4c985d2ff728eddb90158a59
Ollie <c0087087@newcastle.ac.uk>
2023-07-23 12:18:19 +0700

    Trying to switch image using mouse events

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,34 +1,52 @@
-import React, {useState} from 'react';
-
+import React, { useState } from 'react';
 import './Navbar.css';
 import logojpg from '../../assets/logo.jpg';
 import gradientImage from '../../assets/gradient.png';
+import portfolio from '../../assets/Portfolio_hover.gif'
+
+
 const Navbar = () => {
+  const [about, setContent] = useState('');
+  
+  const switch_image = (newContent) => {
+    setContent(newContent);
+  };
+
 
 
   return (
-   <div className='ow_navbar'>
-    
-    <div className='ow_navbar-links'>
-      <div className='ow_navbar-links-logo'>
-        <img src={logojpg} alt='logo' className='logo-image' />
-      </div>
-      <div className='ow_navbar-links-container'>
-      <p><a href='#portfolio'  >.portfolio</a></p>
-      <p><a href='#about'  >.about</a></p>
-      <p><a href='#skills'  >.skills</a></p>
-      <p><a href='#experience'  >.experience</a></p>
-      <p><a href='#contact'  >.contacts</a></p>
-      </div>
-      <div className='background'>
-        <img src={gradientImage} alt='Gradient' className='gradient-image' />
-        
+    <div className='ow_navbar'>
+
+      <div className='ow_navbar-links'>
+        <div className='ow_navbar-links-logo'>
+          <img src={logojpg} alt='logo' className='logo-image' />
+        </div>
+        <div className='ow_navbar-links-container'>
+          <p onMouseOver={() => switch_image(<img src={portfolio} alt="portfolio" />)}>
+            <a href='#portfolio'  >.portfolio</a>
+          </p>
+          <p onMouseOver={switch_image}>
+            <a href='#about'  >.about</a>
+            </p>
+          <p onMouseOver={switch_image}>
+            <a href='#skills'  >.skills</a>
+            </p>
+          <p onMouseOver={switch_image}>
+            <a href='#experience'  >.experience</a>
+            </p>
+          <p onMouseOver={switch_image}>
+            <a href='#contact'  >.contacts</a>
+            </p>
+        </div>
+        <div className='background'>
+          <img src={gradientImage} alt='Gradient' className='gradient-image' />
+
+        </div>
       </div>
     </div>
-   </div>
-);
+  );
 };
-  
+
 
 
 export default Navbar
--- src/containers/Hero Section/Hero_section.jsx
+++ src/containers/Hero Section/Hero_section.jsx
@@ -4,8 +4,10 @@ import monitor from '../../assets/monitor.png';
 import '../../animations/animation.js' // Import the animation file
 // import monitorimage from '../../assets/blob1.svg'
 import about from '../../assets/About_me.gif'
+import portfolio from '../../assets/Portfolio_hover.gif'
 
 const Hero_section = () => {
+  
   return (
     <div className='ow_hero-section-padding'>
       <div className='ow_hero-section-content'>
@@ -22,8 +24,8 @@ const Hero_section = () => {
         </h1>
         <div className='ow_hero-section-monitor-padding'>
           <div className='ow_hero-section-monitor'>
-            {/* <img src={monitor} alt='monitor' class="monitor-image" /> */}
-            <img src={about} alt='about' class="about-gif"/>
+            {/* <img src={monitor} alt='monitor' class="monitor-image" />  */}
+            <img src={about} alt='about' class="about-gif"/> 
           </div>
         </div>
       </div>
@@ -31,44 +33,5 @@ const Hero_section = () => {
   );
 };
 
-// scripts.js
-const imageContainer = document.querySelector('.ow_hero-section-monitor');
-const image = imageContainer.querySelector('img');
-
-const links = document.querySelectorAll('ow_navbar-links-container a');
-
-links.forEach(link => {
-  link.addEventListener('mouseover', () => {
-    const newImagePath = getNewImagePath(link.textContent);
-    if (newImagePath) {
-      image.src = newImagePath;
-    }
-  });
-
-  link.addEventListener('mouseout', () => {
-    // Change the image back to the default image on mouseout
-    image.src = 'C:/Users/james/personal-website-ow/src/assets/About_me.gif';
-  });
-});
-
-function getNewImagePath(linkText) {
-  // Implement your logic to determine the new image path based on the link text
-  // For example, you can use a switch statement or an object with link-text-to-image-path mappings
-  switch (linkText) {
-    case '.portfolio':
-      return 'C:/Users/james/personal-website-ow/src/assets/Portfolio_hover.gif';
-    case '.about':
-      return 'path-to-about-image.jpg';
-    case '.skills':
-      return 'path-to-services-image.jpg';
-    case '.experience':
-      return 'path-to-services-image.jpg';  
-    case '.contacts':
-      return 'path-to-services-image.jpg';
-    // Add more cases for additional links
-    default:
-      return null;
-  }
-}
 
 export default Hero_section;
