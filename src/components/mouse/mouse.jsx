46011599ace097446897f97cc794dccdeaa0a5eb
Ollie <c0087087@newcastle.ac.uk>
2023-07-24 16:21:52 +0700

    MOUSE GLOW EFFECT WORKS :D

--- src/components/mouse/mouse.jsx
+++ src/components/mouse/mouse.jsx
@@ -1,10 +1,43 @@
-import React from 'react';
-import './mouse.css';
+import React, { useEffect } from 'react';
+import './Mouse.css';
+import 'animate.css';
 
-const mouse = () => {
-  return (
-    <div className='blob'></div>
-  )
-}
+const Mouse = () => {
+    useEffect(() => {
+        const blob = document.getElementById("blob");
+        const blobRect = blob.getBoundingClientRect();
 
-export default mouse
+        const updateBlobPosition = (event) => {
+            const { clientX, clientY } = event;
+
+            const left = clientX - blobRect.width / 2;
+            const top = clientY - blobRect.height / 2;
+
+            blob.animate(
+                {
+                    left: `${left}px`,
+                    top: `${top}px`
+                },
+                {
+                    duration: 3000,
+                    fill: "forwards"
+                }
+            );
+        };
+
+        window.addEventListener('pointermove', updateBlobPosition);
+
+        return () => {
+            window.removeEventListener('pointermove', updateBlobPosition);
+        };
+    }, []);
+
+    return (
+        <>
+            <div className="blur"></div>
+            <div className="blob animate__slower" id="blob"></div>
+        </>
+    );
+};
+
+export default Mouse;
