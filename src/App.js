bdfc449a6f82f0cc7321cec08d662f4351a82699
Ollie <c0087087@newcastle.ac.uk>
2023-07-25 12:42:03 +0700

    trying to implement a scrollbar

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import React, {useState} from 'react';
-import  { Navbar,Mouse } from './components';
-import { Hero_section, About_me, Project_section  } from './containers';
+import React from 'react';
+import { Navbar, Mouse, ScrollBar } from './components';
+import { Hero_section, About_me, Project_section } from './containers';
 import './App.css';
-
+import { Scrollbars } from 'react-custom-scrollbars-2';
 
 const App = () => {
   return (
     <div className='App'>
       <div className='background-colour'>
-        <Mouse></Mouse>
-        <Navbar/>
-        <Hero_section/>
-        <About_me/>
-        
-        
+        <Mouse />
+        <Navbar />
+        <ScrollBar >
+          <Hero_section />
+          {/* Add other content here */}
+          {/* <About_me /> */}
+          <Project_section />
+        </ScrollBar>
       </div>
     </div>
-    
-  )
-}
+  );
+};
 
-export default App
+export default App;
--- src/components/index.js
+++ src/components/index.js
@@ -1,2 +1,3 @@
 export {default as Navbar} from './navbar/Navbar'
 export {default as Mouse } from './mouse/Mouse'
+export {default as ScrollBar } from './scrollbar/ScrollBar'
