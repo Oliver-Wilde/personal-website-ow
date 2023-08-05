61e0b3d48c5498cac7d5add3e78021af0ac6294e
Ollie <c0087087@newcastle.ac.uk>
2023-08-05 17:12:02 +0700

    Basic Skills section

--- src/containers/Skills Section/skills_section.jsx
+++ src/containers/Skills Section/skills_section.jsx
@@ -1,13 +1,26 @@
 import React from 'react';
 import './skills_section.css';
 import python from '../../assets/python hover.png';
+import java from '../../assets/java hover.png';
+import react from '../../assets/react hover.png';
+import js from '../../assets/js hover.png';
+import html from '../../assets/html hover.png';
+import css from '../../assets/css hover.png';
 import Card from '../../components/card/card'; // Import the Card component
 
 const Skills_section = () => {
   return (
-    <div className='card-padding'>
-      <Card image={python} />
-      {/* You can add more cards here with different images */}
+    <div id="skills" className='card-padding'>
+      <div className='card-background-bloo'>
+        <div className='card-container'>
+          <Card image={python} />
+          <Card image={java} />
+          <Card image={react}/>
+          <Card image={html}/>
+          <Card image={css}/>
+          <Card image={js}/>
+        </div>
+      </div>
     </div>
   );
 };
