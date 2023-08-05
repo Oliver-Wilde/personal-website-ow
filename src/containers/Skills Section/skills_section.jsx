e27c0f56dbc3ea352c6b6ab35e8295014bcf91c2
Ollie <c0087087@newcastle.ac.uk>
2023-08-05 15:18:36 +0700

    saving to test

--- src/containers/Skills Section/skills_section.jsx
+++ src/containers/Skills Section/skills_section.jsx
@@ -1,16 +1,15 @@
-import React from 'react'
-import './skills_section.css'
-import python from '../../assets/python hover.png'
+import React from 'react';
+import './skills_section.css';
+import python from '../../assets/python hover.png';
+import Card from '../../components/card/card'; // Import the Card component
 
 const Skills_section = () => {
   return (
     <div className='card-padding'>
-      <div className='card'>
-        <img src={python} alt="Python" />
-
-      </div>
+      <Card image={python} />
+      {/* You can add more cards here with different images */}
     </div>
-  )
-}
+  );
+};
 
 export default Skills_section;
