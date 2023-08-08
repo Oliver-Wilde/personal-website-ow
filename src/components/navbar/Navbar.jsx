e7a918c30bcf967b42fafad445ab7e0b2ad13539
Ollie <c0087087@newcastle.ac.uk>
2023-08-08 15:37:22 +0700

    Updated media queries for navbar mobile devices

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react';
+import { RiMenu3Line, RiCloseLine } from 'react-icons/ri';
 import './Navbar.css';
 import logojpg from '../../assets/logo.jpg';
 import gradientImage from '../../assets/gradient.png';
@@ -10,6 +11,7 @@ import about from '../../assets/About_me.gif';
 import defaultimage from '../../assets/default.gif';
 
 const Navbar = () => {
+  const [toggleMenu, setToggleMenu] = useState(false);
   const [hoverContent, setHoverContent] = useState(null);
 
   const switchImage = (newContent) => {
@@ -20,7 +22,7 @@ const Navbar = () => {
     setHoverContent(null);
   };
 
-  
+
 
   return (
     <div className='ow_navbar'>
@@ -48,6 +50,34 @@ const Navbar = () => {
         <div className='background'>
           <img src={gradientImage} alt='Gradient' className='gradient-image' />
         </div>
+        <div className='ow_navbar-menu'>
+        {toggleMenu
+          ? <RiCloseLine color='#fff' size={27} onClick={() => setToggleMenu(false)} />
+          : <RiMenu3Line color='#fff' size={27} onClick={() => setToggleMenu(true)} />
+        }
+        {toggleMenu && (
+          <div className='ow_navbar-menu-container scale-up-centre '>
+            <div className='ow_navbar-menu-container-links scale-up-centre'>
+            <p onMouseEnter={() => switchImage(<img src={portfolio} alt="portfolio" />)} onMouseLeave={resetContent}>
+            <a href='#portfolio'>.portfolio</a>
+          </p>
+          <p onMouseEnter={() => switchImage(<img src={about} alt="about" />)} onMouseLeave={resetContent}>
+            <a href='#about'>.about</a>
+          </p>
+          <p onMouseEnter={() => switchImage(<img src={skills} alt="skills" />)} onMouseLeave={resetContent}>
+            <a href='#skills'>.skills</a>
+          </p>
+          <p onMouseEnter={() => switchImage(<img src={experience} alt="experience" />)} onMouseLeave={resetContent}>
+            <a href='#experience'>.experience</a>
+          </p>
+          <p onMouseEnter={() => switchImage(<img src={contact} alt="contact" />)} onMouseLeave={resetContent}>
+            <a href='#contact'>.contacts</a>
+          </p>
+            </div>
+          </div>
+        )}
+
+      </div>
       </div>
       {/* Conditionally render the default GIF container */}
       {!hoverContent && (
@@ -61,6 +91,7 @@ const Navbar = () => {
           {hoverContent}
         </div>
       )}
+      
     </div>
   );
 };
