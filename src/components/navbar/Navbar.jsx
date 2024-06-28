e18efe42760675d304dc45e9d27747cc129fdb29
Ollie <c0087087@newcastle.ac.uk>
2024-06-28 07:06:37 +0100

    UPDATED switch gif on HOVER within navbar

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -2,9 +2,23 @@ import React, { useState } from 'react';
 import { RiMenu3Line, RiCloseLine } from 'react-icons/ri';
 import './Navbar.css';
 import logojpg from '../../assets/logo.jpg';
+import gradientImage from '../../assets/gradient.png';
+import portfolio from '../../assets/Portfolio_hover.gif';
+import skills from '../../assets/skills.gif';
+import experience from '../../assets/Experience_hover.gif';
+import contact from '../../assets/contact_hover.gif';
+import about from '../../assets/About_me.gif';
 
-const Navbar = () => {
+const Navbar = ({ setHoverGif }) => {
   const [toggleMenu, setToggleMenu] = useState(false);
+
+  const handleMouseEnter = (gif) => {
+    setHoverGif(gif);
+  };
+
+  const handleMouseLeave = () => {
+    setHoverGif(null);
+  };
 
   return (
     <div className='ow_navbar'>
@@ -13,43 +27,45 @@ const Navbar = () => {
           <img src={logojpg} alt='logo' className='logo-image' />
         </div>
         <div className='ow_navbar-links-container'>
-          <p>
+          <p onMouseEnter={() => handleMouseEnter(portfolio)} onMouseLeave={handleMouseLeave}>
             <a href='#portfolio'>.portfolio</a>
           </p>
-          <p>
+          <p onMouseEnter={() => handleMouseEnter(about)} onMouseLeave={handleMouseLeave}>
             <a href='#about'>.about</a>
           </p>
-          <p>
+          <p onMouseEnter={() => handleMouseEnter(skills)} onMouseLeave={handleMouseLeave}>
             <a href='#skills'>.skills</a>
           </p>
-          <p>
+          <p onMouseEnter={() => handleMouseEnter(experience)} onMouseLeave={handleMouseLeave}>
             <a href='#experience'>.experience</a>
           </p>
-          <p>
+          <p onMouseEnter={() => handleMouseEnter(contact)} onMouseLeave={handleMouseLeave}>
             <a href='#contact'>.contacts</a>
           </p>
+        </div>
+        <div className='background'>
+          <img src={gradientImage} alt='Gradient' className='gradient-image' />
         </div>
         <div className='ow_navbar-menu'>
           {toggleMenu
             ? <RiCloseLine color='#fff' size={27} onClick={() => setToggleMenu(false)} />
-            : <RiMenu3Line color='#fff' size={27} onClick={() => setToggleMenu(true)} />
-          }
+            : <RiMenu3Line color='#fff' size={27} onClick={() => setToggleMenu(true)} />}
           {toggleMenu && (
             <div className='ow_navbar-menu-container scale-up-centre '>
               <div className='ow_navbar-menu-container-links scale-up-centre'>
-                <p>
+                <p onMouseEnter={() => handleMouseEnter(portfolio)} onMouseLeave={handleMouseLeave}>
                   <a href='#portfolio'>.portfolio</a>
                 </p>
-                <p>
+                <p onMouseEnter={() => handleMouseEnter(about)} onMouseLeave={handleMouseLeave}>
                   <a href='#about'>.about</a>
                 </p>
-                <p>
+                <p onMouseEnter={() => handleMouseEnter(skills)} onMouseLeave={handleMouseLeave}>
                   <a href='#skills'>.skills</a>
                 </p>
-                <p>
+                <p onMouseEnter={() => handleMouseEnter(experience)} onMouseLeave={handleMouseLeave}>
                   <a href='#experience'>.experience</a>
                 </p>
-                <p>
+                <p onMouseEnter={() => handleMouseEnter(contact)} onMouseLeave={handleMouseLeave}>
                   <a href='#contact'>.contacts</a>
                 </p>
               </div>
