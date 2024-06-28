10b8589617434ce1830609a15d1376380422c909
Ollie <c0087087@newcastle.ac.uk>
2024-06-28 11:35:13 +0100

    ADDED prototype of portfolio page

--- src/containers/Portfolio/portfolio.jsx
+++ src/containers/Portfolio/portfolio.jsx
@@ -1,42 +1,39 @@
-import React, { useState } from 'react';
+import React from 'react';
+import ProjectCard from '../../components/projectcard/ProjectCard';
 import './portfolio.css';
+import torahimage from '../../assets/projects/torah.png';
+
+const projects = [
+    {
+        title: 'Torah Code Experiment',
+        description: 'Enter a name and you will get an output of known context',
+        image: torahimage,
+        link: 'http://example.com/project1'
+    },
+    {
+        title: 'Project 2',
+        description: 'Description for project 2',
+        image: 'path/to/project2-image.jpg',
+        link: 'http://example.com/project2'
+    },
+    // Add more projects here
+];
 
 const Portfolio = () => {
-    const [selectedBubble, setSelectedBubble] = useState(null);
-
-    const items = [
-        { id: 1, title: "Project 1", description: "Description for project 1", imageUrl: "path/to/image1.jpg" },
-        { id: 2, title: "Project 2", description: "Description for project 2", imageUrl: "path/to/image2.jpg" },
-        // Add more items as needed
-    ];
-
-    const handleBubbleClick = (item) => {
-        setSelectedBubble(item);
-    };
-
-    const handleClose = () => {
-        setSelectedBubble(null);
-    };
-
     return (
-        <div className="portfolio">
-            {items.map(item => (
-                <div
-                    key={item.id}
-                    className={`bubble ${selectedBubble && selectedBubble.id === item.id ? 'expanded' : ''}`}
-                    onClick={() => handleBubbleClick(item)}
-                >
-                    <img src={item.imageUrl} alt={item.title} />
-                </div>
-            ))}
-            {selectedBubble && (
-                <div className="info-panel active" onClick={handleClose}>
-                    <div className="info-content">
-                        <h2>{selectedBubble.title}</h2>
-                        <p>{selectedBubble.description}</p>
-                    </div>
-                </div>
-            )}
+        <div className="portfolio-section">
+            <h2 className="portfolio-title">My Projects</h2>
+            <div className="portfolio-grid">
+                {projects.map((project, index) => (
+                    <ProjectCard
+                        key={index}
+                        title={project.title}
+                        description={project.description}
+                        image={project.image}
+                        link={project.link}
+                    />
+                ))}
+            </div>
         </div>
     );
 };
