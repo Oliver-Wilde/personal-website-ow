fe8c9a841af53e277924177f54c2a9db4d0b4848
Ollie <c0087087@newcastle.ac.uk>
2024-06-29 10:13:23 +0100

    UPDATED portfolio structure

--- src/containers/Portfolio/portfolio.jsx
+++ src/containers/Portfolio/portfolio.jsx
@@ -8,13 +8,31 @@ const projects = [
         title: 'Torah Code Experiment',
         description: 'Enter a name and you will get an output of known context',
         image: torahimage,
-        link: 'http://example.com/project1'
+        link: 'http://example.com/project1',
+        role: 'Lead Developer',
+        teamSize: 'N/A',
+        engine: 'Custom',
+        responsibilities: [
+            'Designed and developed the algorithm',
+            'Implemented the front-end interface',
+            'Conducted testing and optimization'
+        ],
+        projectType: 'Project Page'
     },
     {
         title: 'Project 2',
         description: 'Description for project 2',
         image: 'path/to/project2-image.jpg',
-        link: 'http://example.com/project2'
+        link: 'http://example.com/project2',
+        role: 'Software Engineer',
+        teamSize: 4,
+        engine: 'Unity',
+        responsibilities: [
+            'Game mechanics design',
+            'Level design',
+            'Bug fixing and optimization'
+        ],
+        projectType: 'Store Page'
     },
     // Add more projects here
 ];
@@ -31,6 +49,11 @@ const Portfolio = () => {
                         description={project.description}
                         image={project.image}
                         link={project.link}
+                        role={project.role}
+                        teamSize={project.teamSize}
+                        engine={project.engine}
+                        responsibilities={project.responsibilities}
+                        projectType={project.projectType}
                     />
                 ))}
             </div>
