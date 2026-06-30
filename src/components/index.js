389280b3f304161d833c08308eb149cc1c3ed86d
Ollie <c0087087@newcastle.ac.uk>
2026-06-30 18:47:44 +0700

    refactor: fix navbar import casing

--- src/components/index.js
+++ src/components/index.js
@@ -1,4 +1,4 @@
-export { default as Navbar } from './Navbar/Navbar';
+export { default as Navbar } from './navbar/Navbar';
 export { default as Mouse } from './mouse/Mouse';
 export { default as ScrollBar } from './scrollbar/ScrollBar';
 export { default as ProjectCard } from './projectcard/ProjectCard';
