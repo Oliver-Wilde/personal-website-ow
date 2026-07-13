c7de2d4d0822bbacf73286258f213920efc1a12d
Ollie <c0087087@newcastle.ac.uk>
2026-07-13 16:59:18 +0700

    chore: remove legacy portfolio implementation

--- src/components/index.js
+++
@@ -1,3 +0,0 @@
-export { default as Navbar } from './navbar/Navbar';
-export { default as Mouse } from './mouse/Mouse';
-export { default as ScrollBar } from './scrollbar/ScrollBar';
--- src/containers/index.js
+++
@@ -1,5 +0,0 @@
-export { default as HeroSection } from './Hero Section/Hero_section';
-export { default as AboutSection } from './About Me/About_me';
-export { default as ProjectsSection } from './Project Section/project_section';
-export { default as SkillsSection } from './Skills Section/skills_section';
-export { default as ContactSection } from './Contact_section/ContactSection';
